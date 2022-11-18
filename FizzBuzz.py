ede51c6f5654f83c0016a634a02ffe48120e1465
musaaibnatif <104182516+musaaibnatif@users.noreply.github.com>
2022-11-17 21:15:39 -0800

    Add files via upload

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,8 @@
-array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-
-for i in array:
-    if(i%5 == 0 and i%3 == 0):
-        print("FizzBuzz")
-    elif (i%5 == 0):
-        print("Buzz")
-    elif(i%3 == 0):
-        print("Fizz")
+for i in range(1,100):
+    fizzy = ""
+    if(i%3 == 0):
+        fizzy += "Fizz"
+    if (i%5 == 0):
+        fizzy += "Buzz"
+    print(fizzy)
+    
